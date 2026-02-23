745e4d83f4d7402764dcaed42a2c163e1a218a49
Ariana I. <git@arianaivan.com>
2026-02-23 10:38:23 -0800

    Update bg color to a warmer dark blue instead of black

--- app/pitchdeck/ScrollFrame.tsx
+++ app/pitchdeck/ScrollFrame.tsx
@@ -8,7 +8,7 @@ type ScrollFrameProps = {
 export function ScrollFrame({ children, className = '' }: ScrollFrameProps) {
   return (
     <div
-      className={`overflow-hidden rounded-xl border border-white/10 bg-white/[0.02] shadow-[inset_0_0_0_1px_rgba(255,255,255,0.04)] ${className}`}
+      className={`overflow-hidden rounded-xl border border-white/[0.08] bg-white/[0.04] shadow-[inset_0_0_0_1px_rgba(255,255,255,0.05)] ${className}`}
     >
       <div className="flex h-[400px] snap-x snap-mandatory overflow-x-auto overflow-y-hidden scroll-smooth md:h-[480px] [scrollbar-width:thin] [&::-webkit-scrollbar]:h-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-white/20 [&::-webkit-scrollbar-track]:bg-transparent">
         {children}
