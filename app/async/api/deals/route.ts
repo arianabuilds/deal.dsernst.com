32aee64adeb893ca2c5b6d61e368ffa7cac880c6
David Ernst <git@dsernst.com>
2026-04-20 02:43:37 -0700

    /deals endpoint

---
+++ app/async/api/deals/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from 'next/server'
+
+import { getDb, initDb } from '../db'
+
+// Initialize database on module load
+let dbInitialized = false
+const initPromise = initDb().then(() => (dbInitialized = true))
+
+export async function GET() {
+  const records = await getAllRecords()
+  const cleaned = records.map((record, index) => {
+    return (
+      index +
+      1 +
+      '. ' +
+      new Date(record.created_at * 1000).toLocaleString() +
+      ': ' +
+      safeRound(JSON.parse(record.result).result)
+    )
+  })
+  return NextResponse.json(cleaned.reverse())
+}
+
+export async function getAllRecords() {
+  if (!dbInitialized) await initPromise
+
+  const db = getDb()
+
+  return (await db.execute({ sql: 'SELECT * FROM payloads' })).rows as unknown as {
+    created_at: number
+    result: string
+  }[]
+}
+
+function safeRound(value: null | number) {
+  if (value === null) return null
+  return Math.round(value * 100) / 100
+}
