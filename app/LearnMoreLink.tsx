0ecd3a448c1100ab235c870b459059f27dd584b9
David Ernst <git@dsernst.com>
2026-05-04 23:05:58 -0700

    Update LearnMore link

--- app/LearnMoreLink.tsx
+++ app/LearnMoreLink.tsx
@@ -1,7 +1,7 @@
 export const LearnMoreLink = () => (
   <a
     className="text-sm text-gray-400 mt-16 block hover:underline"
-    href="https://github.com/dsernst/deal.dsernst.com#dealdsernstcom"
+    href="https://github.com/dsernst/deal.siv.org#dealsivorg"
     target="_blank"
   >
     Learn more
