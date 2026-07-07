6bdeeb872b8d1972dfe30f203c13a770cae4a8ea
Ariana I. <git@arianaivan.com>
2026-07-07 14:52:28 -0700

    Add SiteHeader as component to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 
 import './globals.css'
 import { description, title } from './constants'
+import { SiteHeader } from './SiteHeader'
 
 export const metadata: Metadata = { description, title }
 
@@ -17,8 +18,7 @@ export default function RootLayout({
           className="min-h-screen p-8 pt-4 flex flex-col items-center justify-center text-center"
           style={{ minHeight: '100dvh' }} // ignore iOS bottom bar
         >
-          <h1 className="text-4xl font-bold mb-1">{title}</h1>
-          <p className="text-lg text-gray-400 mb-8">{description}</p>
+          <SiteHeader />
 
           {/* Content */}
           {children}
